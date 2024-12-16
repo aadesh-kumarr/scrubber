4350c524166c1f7590ffac3190d406263173899c
Aadesh Kumar <116622502+aadesh-kumarr@users.noreply.github.com>
2024-12-16 16:44:12 +0530

    Update authorization.ts

--- src/lib/authorization.ts
+++ src/lib/authorization.ts
@@ -1,7 +1,7 @@
 "use server";
 
 function authorisation(email: string){
-    if(email === 'aadeshk306@gmail.com' || email === 'subodh.sabbarwal@gmail.com'){
+    if(email === 'aadeshk306@gmail.com' || email === 'subodh.sabbarwal@gmail.com' || 'itstyagi0001@gmail.com'){
         return true;
     } else {
         return false;
